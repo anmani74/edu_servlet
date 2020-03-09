d91d9894a68b23ab5f2aa5412e1c4eaf36345314
jwm <anmani74@naver.com>
2020-03-09 17:55:02 +0900

    jsp/servlet day-5.2

--- src/com/newlecture/web/Calc2.java
+++ src/com/newlecture/web/Calc2.java
@@ -1,6 +1,8 @@
 package com.newlecture.web;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,27 +14,37 @@ import javax.servlet.http.HttpServletResponse;
 public class Calc2 extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//서블릿컨텍스트 만들기
+		ServletContext application = request.getServletContext();
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
-		String x_ = request.getParameter("x");
-		String y_ = request.getParameter("y");
+		String v_ = request.getParameter("v");
 		String op = request.getParameter("operator");
 		
-		int x = 0;
-		int y = 0;
+		int v = 0;
+		if(!v_.equals("")) v = Integer.parseInt(v_);
 		
-		if(!x_.contentEquals("")) x = Integer.parseInt(x_);
-		if(!y_.contentEquals("")) y = Integer.parseInt(y_);
+		if(op.equals("=")) {
+			// 입력된 op 값이 = 이면 입력된 값 계산하기
+			int x = (Integer)application.getAttribute("value");
+			int y = v;
+			String operator = (String)application.getAttribute("op");
+			
+			int result = 0;
+			
+			if(operator.equals("+"))
+				result = x + y;
+			else
+				result = x - y;
+			
+			response.getWriter().printf("result is %d\n", result);
+		}
+		else {//저장하기
+			application.setAttribute("value", v);
+			application.setAttribute("op", op);
+		}
 		
-		int result = 0;
-				
-		if(op.contentEquals("덧셈"))
-			result = x + y;
-		else
-			result = x - y;
-		
-		response.getWriter().printf("result is %d\n", result);
 	}
 
 	
